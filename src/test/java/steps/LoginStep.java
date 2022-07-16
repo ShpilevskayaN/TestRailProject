d79be3bcc3d31d8e7879c477294d15ff7d8f455e
Shpilevskaya Natasha <sh-natasha1989@yandex.ru>
2022-07-16 21:22:45 +0300

    15/07

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -2,6 +2,7 @@ package steps;
 
 import baseEntities.BaseStep;
 import configuration.ReadProperties;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import pages.DashboardPage;
 import pages.LoginPage;
@@ -15,15 +16,43 @@ public class LoginStep extends BaseStep {
         login(email, psw);
         return dashboardPage;
     }
+
     public LoginPage incorrectLogin(String email, String psw){
         login(email, psw);
         return loginPage;
     }
+
     private void login(String email, String psw){
         loginPage.getEmailInput().sendKeys(email);
         loginPage.getPswInput().sendKeys(psw);
         loginPage.getLogInButton().click();
     }
+
+    public LoginPage boundaries251() {
+        String random251 = RandomStringUtils.randomAlphabetic(251);
+        login(random251, ReadProperties.password());
+
+        return loginPage;
+
+    }
+
+
+    public LoginPage boundaries250() {
+        String random250 = RandomStringUtils.randomAlphabetic(250);
+        login(ReadProperties.username(), random250);
+        return loginPage;}
+
+
+    public LoginPage boundaries249() {
+
+
+        String random249= RandomStringUtils.randomAlphabetic(249);
+        login(random249, ReadProperties.password());
+        return loginPage;
+
+
+    }
+
     public LoginPage logout(){
         return loginPage;
 
