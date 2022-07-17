e7b0f45d14676ff11d36d692fbce634af0cfcb3f
Shpilevskaya Natasha <sh-natasha1989@yandex.ru>
2022-07-17 19:55:15 +0300

    17/07

--- src/test/java/tests/ui/AddProjectTest.java
+++ src/test/java/tests/ui/AddProjectTest.java
@@ -1,21 +1,27 @@
 package tests;
 
 import baseEntities.BaseTest;
+import common.WaitsService;
 import configuration.ReadProperties;
+import io.qameta.allure.Feature;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class AddProjectTest extends BaseTest {
+
+    @Feature("тест на загрузку файла")
     @Test
-    public void addProjectTest() {
-                loginStep.successLogin(ReadProperties.username(),ReadProperties.password());
-                navigationSteps.navigateToAddNewProjectPage();
-                Assert.assertEquals(addNewProjectSteps.addNewProject("New Project","Some Text").getSuccessMessage().getText(),"Successfully added the new project.");
-            }
-        }
+    public void fileUploadTest() {
+        driver.get("https://shpilka.testrail.io/index.php?/cases/add/1");
 
-
-
-
+        WaitsService wait = new WaitsService(driver, Duration.ofSeconds(20));
+        WebElement fileUploadPath = wait.waitForVisibility(driver.findElement(By.id("entityAttachmentListEmptyIcon")));
+        fileUploadPath.sendKeys("attachment-library-add-icon dz-clickable");
+        wait.waitForVisibility(driver.findElement(By.id("file-submit"))).submit();
+    }
 
 }
